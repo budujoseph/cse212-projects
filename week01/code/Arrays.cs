0021b37d497e44c8903c000bc13f61c0411ab691
budujoseph <budu.joseph95@gmail.com>
2025-10-31 22:29:01 +0000

    wk1 assignments

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,12 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        double[] multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +33,13 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        if(data.Count == 0)
+        return;
+
+        int n = data.Count;
+        List<int> tail = data.GetRange(n - amount, amount);
+        data.InsertRange(0, tail);
+        data.RemoveRange(n, amount);
     }
 }
